71d9304c6f826a8dd5cf329895cb89db73573031
MGodmonkey <wpqds666@163.com>
2024-06-13 11:14:32 +0800

    修改默认主页为照片墙

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,15 @@
 import Link from "next/link";
-import { GetServerSideProps } from 'next';
-
-// export default function Home() {
-//   return (
-//     <center className="p-12 space-y-5">
-//       <Link className="text-5xl font-serif" href={"/gallery-wall"}>
-//         Gallery Wall
-//       </Link>
-//       <div></div>
-//       <Link className="text-5xl font-serif" href={"/vacation-countdown"}>
-//         Vacation Countdown
-//       </Link>
-//     </center>
-//   );
-// }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  context.res.setHeader('Location', '/gallery-wall');
-  context.res.statusCode = 302;
-  return { props: {} };
-};
 
 export default function Home() {
-  return null;
+  return (
+    <center className="p-12 space-y-5">
+      <Link className="text-5xl font-serif" href={"/gallery-wall"}>
+        Gallery Wall
+      </Link>
+      <div></div>
+      <Link className="text-5xl font-serif" href={"/vacation-countdown"}>
+        Vacation Countdown
+      </Link>
+    </center>
+  );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/gallery-wall',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
