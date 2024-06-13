555f0cfa1caa7658a0787d96330606380580afaf
MGodmonkey <wpqds666@163.com>
2024-06-13 11:01:24 +0800

    将默认主页修改为照片墙

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
 import Link from "next/link";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
-  return (
-    <center className="p-12 space-y-5">
-      <Link className="text-5xl font-serif" href={"/gallery-wall"}>
-        Gallery Wall
-      </Link>
-      <div></div>
-      <Link className="text-5xl font-serif" href={"/vacation-countdown"}>
-        Vacation Countdown
-      </Link>
-    </center>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/gallery-wall');
+  }, []);
+
+  return null;
+  // return (
+  //   <center className="p-12 space-y-5">
+  //     <Link className="text-5xl font-serif" href={"/gallery-wall"}>
+  //       Gallery Wall
+  //     </Link>
+  //     <div></div>
+  //     <Link className="text-5xl font-serif" href={"/vacation-countdown"}>
+  //       Vacation Countdown
+  //     </Link>
+  //   </center>
+  // );
 }
