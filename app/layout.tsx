208241ece8a71ff15cca1442fb113aa4bc45dd34
MGodmonkey <wpqds666@163.com>
2024-06-13 19:28:45 +0800

    修改标题

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Nice Shit",
-  description: "Forairaaaaa personal website",
+  title: "Photo Wall",
+  description: "MGodmonkey's photo wall",
 };
 
 export default function RootLayout({
