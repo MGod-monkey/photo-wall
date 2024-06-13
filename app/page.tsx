7e125396673b2088e73f0e9c53a77f91bd31aaff
MGodmonkey <wpqds666@163.com>
2024-06-13 11:07:26 +0800

    修改默认主页为照片墙

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,26 @@
 import Link from "next/link";
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { GetServerSideProps } from 'next';
+
+// export default function Home() {
+//   return (
+//     <center className="p-12 space-y-5">
+//       <Link className="text-5xl font-serif" href={"/gallery-wall"}>
+//         Gallery Wall
+//       </Link>
+//       <div></div>
+//       <Link className="text-5xl font-serif" href={"/vacation-countdown"}>
+//         Vacation Countdown
+//       </Link>
+//     </center>
+//   );
+// }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  context.res.setHeader('Location', '/gallery-wall');
+  context.res.statusCode = 302;
+  return { props: {} };
+};
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace('/gallery-wall');
-  }, []);
-
   return null;
-  // return (
-  //   <center className="p-12 space-y-5">
-  //     <Link className="text-5xl font-serif" href={"/gallery-wall"}>
-  //       Gallery Wall
-  //     </Link>
-  //     <div></div>
-  //     <Link className="text-5xl font-serif" href={"/vacation-countdown"}>
-  //       Vacation Countdown
-  //     </Link>
-  //   </center>
-  // );
 }
