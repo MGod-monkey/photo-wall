f5c635eb7c7871b1acaa38090a12615566349a4d
wpqds666@163.com <MGodmonkey>
2024-10-17 16:29:30 +0800

    修改标题

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Photo Wall",
-  description: "MGodmonkey's photo wall",
+  title: "最伟大的作品",
+  description: "来自杨大画师最伟大的作品展览！！！",
 };
 
 export default function RootLayout({
